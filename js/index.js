d4cece7e3f3d0272725dfa1fdfde8e2193600c7f
unknown <delfdevs@gmail.com>
2019-11-03 19:57:24 -0500

    msgDOM-I

--- js/index.js
+++ js/index.js
@@ -40,3 +40,55 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const links = document.querySelectorAll('nav a')[0].textContent = siteContent['nav']['nav-item-1'];
+const links1 = document.querySelectorAll('nav a')[1].textContent = siteContent['nav']['nav-item-2'];
+const links2 = document.querySelectorAll('nav a')[2].textContent = siteContent['nav']['nav-item-3'];
+const links3 = document.querySelectorAll('nav a')[3].textContent = siteContent['nav']['nav-item-4'];
+const links4 = document.querySelectorAll('nav a')[4].textContent = siteContent['nav']['nav-item-5'];
+const links5 = document.querySelectorAll('nav a')[5].textContent = siteContent['nav']['nav-item-6'];
+
+const color = document.querySelectorAll('nav a')[0].style.color = 'green';
+const color1 = document.querySelectorAll('nav a')[1].style.color = 'green';
+const color2 = document.querySelectorAll('nav a')[2].style.color = 'green';
+const color3 = document.querySelectorAll('nav a')[3].style.color = 'green';
+const color4 = document.querySelectorAll('nav a')[4].style.color = 'green';
+const color5 = document.querySelectorAll('nav a')[5].style.color = 'green';
+
+
+let pic = document.getElementById("cta-img");
+pic.setAttribute('src', siteContent["cta"]["img-src"]);
+
+const dis = document.querySelector('.cta .cta-text h1').textContent = siteContent['cta']['h1'];
+
+const btn = document.querySelector('.cta .cta-text button').textContent = siteContent['cta']['button'];
+
+
+const features = document.querySelectorAll('.top-content .text-content h4')[0].textContent = siteContent['main-content']['features-h4'];
+const featuresContent = document.querySelectorAll('.top-content .text-content P')[0].textContent = siteContent['main-content']['features-content'];
+
+const about = document.querySelectorAll('.top-content .text-content h4')[1].textContent = siteContent['main-content']['about-h4'];
+const aboutContent = document.querySelectorAll('.top-content .text-content P')[1].textContent = siteContent['main-content']['about-content'];
+
+
+let pic2 = document.getElementById("middle-img");
+pic2.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+const  services = document.querySelectorAll('.bottom-content .text-content h4')[0].textContent = siteContent['main-content']['services-h4'];
+const  servicesContent = document.querySelectorAll('.bottom-content .text-content p')[0].textContent = siteContent['main-content']['services-content'];
+
+const product = document.querySelectorAll('.bottom-content .text-content h4')[1].textContent = siteContent['main-content']['product-h4'];
+const  productContent = document.querySelectorAll('.bottom-content .text-content p')[1].textContent = siteContent['main-content']['product-content'];
+
+const  vision = document.querySelectorAll('.bottom-content .text-content h4')[2].textContent = siteContent['main-content']['vision-h4'];
+const  visionContent = document.querySelectorAll('.bottom-content .text-content p')[2].textContent = siteContent['main-content']['vision-content'];
+
+const contact = document.querySelector('.contact h4').textContent = siteContent['contact']['contact-h4'];
+
+const contact1 = document.querySelectorAll('.contact p')[0].textContent = siteContent['contact']['address'];
+const contact2 = document.querySelectorAll('.contact p')[1].textContent = siteContent['contact']['phone'];
+const contact3 = document.querySelectorAll('.contact p')[2].textContent = siteContent['contact']['email'];
+
+const footer = document.querySelector('footer p').textContent = siteContent['footer']['copyright'];
+
+
